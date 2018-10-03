b78c744ec58b6a281717a1e846e9285636b85a99
Prashant B <prashant_bhikhu@hotmail.com>
2018-10-04 02:09:56 +1300

    Changed Template-Driven form to Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -21,6 +22,8 @@ import { ItemDetailsComponent } from './item-details/item-details.component';
     NgbModule.forRoot(),
     BrowserModule,
     HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  password: FormControl = new FormControl('', Validators.required);
+  password_confirm: FormControl = new FormControl('', CustomValidators.equalTo(this.password));
+
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    homePhoneNumber: new FormControl('', Validators.required),
+    mobilePhoneNumber: new FormControl('', Validators.required),
+    email: new FormControl('', CustomValidators.email),
+    password: this.password,
+    password_confirm: this.password_confirm,
+    address_line1: new FormControl('', Validators.required),
+    address_line2: new FormControl(),
+    address_suburb: new FormControl('', Validators.required),
+    address_city: new FormControl('', Validators.required),
+    address_postCode: new FormControl('', [Validators.required, CustomValidators.number, CustomValidators.rangeLength([4, 4])]),
+    card_number: new FormControl('', CustomValidators.creditCard),
+    card_name: new FormControl('', Validators.pattern('[a-zA-Z ]+')),
+    card_cvv: new FormControl('', [Validators.required, CustomValidators.number, CustomValidators.rangeLength([3, 3])]),
+    card_expiry: new FormControl('',  Validators.pattern('^[0-9]{2}\/[0-9]{4}$')),
+  });
+
 
   constructor() { }
 
   ngOnInit() {
+    this.form.valueChanges.subscribe((val) => {
+      console.log(val);
+    });
   }
 
+  clearAllFields() {
+    this.form.reset();
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
