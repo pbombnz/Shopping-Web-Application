9fac7d51be72310f1a278937c9ab6c7c548de921
iodachi <a547601908@gmail.com>
2018-10-15 16:08:15 +1300

    added sql scripts, starting on the restful API

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+var app = express();
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect 
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow ,
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow- Headers');
+  // Pass to next layer of middleware
+  next();
+});
+
+app
+  .use(express.static(path.join(__dirname, 'src')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+  app.get('/users', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      var result = await client.query('SELECT * FROM ' + 'category');
+  
+      if (!result) {
+        //not found
+        return res.json(404, 'No data found');
+      } else {
+        result.rows.forEach(row => {
+          console.log(row);
+        });
+        res.send(result.rows);
+      }
+      client.release();
+  
+    } catch (err) {
+      //bad request
+      console.error(err);
+      res.json(400);
+    }
+  
+    //ok
+    res.json(200);
+  });
---
+++ shop.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users (
+    user_id serial NOT NULL PRIMARY KEY,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    email varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+    address_line1 varchar(100) NOT NULL,
+    address_line2 varchar(100) NOT NULL,
+    address_suburb varchar(100) NOT NULL,
+    address_city varchar(100) NOT NULL,
+    address_postcode integer NOT NULL,
+    phone varchar(30) NOT NULL    
+);
+
+CREATE TABLE orders (
+    order_id serial NOT NULL PRIMARY KEY,
+    user_id serial NOT NULL REFERENCES users,
+    date date NOT NULL,
+    order_status integer NOT NULL,
+
+    CONSTRAINT status CHECK (order_status >= 0 AND order_status <= 2)
+);
+
+CREATE TABLE order_items (
+    order_id serial NOT NULL,
+    item_id serial NOT NULL,
+    price money NOT NULL,
+    quantity integer NOT NULL,
+
+    CONSTRAINT pk PRIMARY KEY (order_id, item_id),
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES
+    orders (order_id) MATCH SIMPLE 
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES
+    items (item_id) MATCH SIMPLE 
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+    item_id serial NOT NULL PRIMARY KEY,
+    item_name varchar(30) NOT NULL,
+    item_description text NOT NULL,
+    item_category serial NOT NULL REFERENCES category (category_id),
+    item_origin varchar(30) NOT NULL,
+    item_price money NOT NULL,
+    item_stock_quantity integer NOT NULL,
+    item_image text NOT NULL
+);
+
+CREATE TABLE category (
+    category_id serial NOT NULL PRIMARY KEY,
+    category_name varchar(30) NOT NULL
+);
+
+INSERT INTO category (category_name) VALUES
+('meat'),
+('fruits'),
+('vegetable');
