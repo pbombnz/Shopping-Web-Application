6c69ec2df918cf06805a60d38beb72696e3c8fd3
Prashant Bhikhu <bhikhupras@olive-olive.ecs.vuw.ac.nz>
2018-10-17 14:34:46 +1300

    Added missing bracket.

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -78,7 +78,7 @@ export class SearchBarComponent implements OnInit {
             return of([]);
           }))
       ),*/
-      tap(() => this.searching = false);
+      tap(() => this.searching = false));
 
   constructor(private router: Router) { }
 
