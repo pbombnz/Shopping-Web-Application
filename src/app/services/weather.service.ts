4ad1e6305b4b317b3e3276cab1f347215ac84105
claudikyle <claudikyle@ecs.vuw.ac.nz>
2018-11-11 17:35:04 +1300

    added comments and fixed typos #34

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,6 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
+/**
+ * This classes utilises OpenWeatherMap's Weather API to get weather data
+ * as part of the external service requirement for recommendations.
+ */
+
 @Injectable({
     providedIn: 'root'
   })
@@ -22,8 +27,8 @@ export class WeatherService {
     public weatherCondition: string;
 
     public constructor(private http: HttpClient) {
+        // APP Key from my account
         this.appId = "d5fe3061eb1bc6a1d724d33ae9417b92";
-        // this.appCode = "APP-CODE-HERE";
         this.weatherData = [];
         this.city = "Wellington";
         this.country = "nz";
@@ -32,15 +37,16 @@ export class WeatherService {
 
     }
 
+    // Do a get request to the weather API
     public getWeather() {
         this.http.get(this.externalURL).subscribe(res =>{
             console.log(res);
             this.weatherData = res;
             this.assessWeatherCondition();
-
         })
     }
 
+    // Weather Condition Codes: https://openweathermap.org/weather-conditions
     private assessWeatherCondition(){
         let weatherID = this.weatherData.weather[0].id
 
@@ -48,9 +54,7 @@ export class WeatherService {
         if(weatherID >= 300 && weatherID < 400){ this.weatherCondition = "Drizzle"; }
         if(weatherID >= 500 && weatherID < 600){ this.weatherCondition = "Rain"; }
         if(weatherID >= 600 && weatherID < 700){ this.weatherCondition = "Snow"; }
-        if(weatherID >= 200 && weatherID < 300){ this.weatherCondition = "Thunderstorm"; }
-        if(weatherID >= 200 && weatherID < 300){ this.weatherCondition = "Atmosphere"; }
-        if(weatherID >= 200 && weatherID < 300){ this.weatherCondition = "Thunderstorm"; }
+        if(weatherID >= 700 && weatherID < 800){ this.weatherCondition = "Atmosphere"; }
         if(weatherID == 800){ this.weatherCondition = "Clear"; }
         if(weatherID > 800 && weatherID < 900){ this.weatherCondition = "Clouds"; }
         
