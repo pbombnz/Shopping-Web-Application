20ae384ac8d9504dc05957c8c8fe5ed225abad1e
iodachi <a547601908@gmail.com>
2018-11-11 21:01:58 +1300

    payment page cart

--- src/app/payment-page/payment-page.component.ts
+++ src/app/payment-page/payment-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CartPageService } from '../cart-page/cart-page.service';
 import { CartItem } from '../cart-page/cart-item';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
 
 @Component({
   selector: 'app-payment-page',
@@ -11,10 +13,27 @@ import { CartItem } from '../cart-page/cart-item';
 export class PaymentPageComponent implements OnInit {
   cartItems: CartItem[];
   total: number = 0;
+  numItems: number = 0;
+
+  form: FormGroup = new FormGroup({
+    first_name: new FormControl('', Validators.required),
+    last_name: new FormControl('', Validators.required),
+    phone : new FormControl('', Validators.required),
+    address_line1: new FormControl('', Validators.required),
+    address_line2: new FormControl(''),
+    address_suburb: new FormControl('', Validators.required),
+    address_city: new FormControl('', Validators.required),
+    address_postcode: new FormControl('', [Validators.required, CustomValidators.digits, CustomValidators.rangeLength([4, 4])]),
+    card_number: new FormControl('', [Validators.required, CustomValidators.creditCard]),
+    card_name: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]),
+    card_cvv: new FormControl('', [Validators.required, CustomValidators.digits, CustomValidators.rangeLength([3, 3])]),
+    card_expiry: new FormControl('',  [Validators.required,  Validators.pattern('^[0-9]{2}\/[0-9]{4}$')]),
+  });
   
   constructor(private router: Router, private cartPageService: CartPageService) { }
 
   ngOnInit() {
+    this.getCartItems();
   }
 
   getCartItems(): void {
@@ -22,6 +41,7 @@ export class PaymentPageComponent implements OnInit {
       .subscribe(items => {
         this.cartItems = items;
         this.calculateTotalPrice();
+        this.numItems = this.cartItems.length;
       })
   }
 
