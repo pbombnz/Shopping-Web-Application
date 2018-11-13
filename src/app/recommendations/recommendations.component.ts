7cd146519171c69f63d1e08cd90020d0a26633ac
claudikyle <claudikyle@ecs.vuw.ac.nz>
2018-11-13 14:38:28 +1300

    fixed spelling mistake and reset weather location for wellington nz

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -30,7 +30,7 @@ export class RecommendationsComponent implements OnInit {
     this.recommendationsStrings.setValue("Snow","Have some beef for stew on this cold day");
     this.recommendationsStrings.setValue("Rain", "Have some beetroot and chicken, it's raining");
     this.recommendationsStrings.setValue("Atmosphere", "Grab some avocado for this atmospheric day");
-    this.recommendationsStrings.setValue("Clouds", "Have some mandarins for this cloudy day, it's good to have vitamen D");
+    this.recommendationsStrings.setValue("Clouds", "Have some mandarins for this cloudy day, it's good to have vitamin D");
     this.recommendationsStrings.setValue("Clear", "It's sunny! Buy meat and veges for a BBQ!");
     this.recommendationsStrings.setValue("Unknown", "We cannot recommend anything, we have no weather data");
     this.recommendationsStrings.setValue("Drizzle", "Sweet corn is great for this drizzling day");
